5880f803c92b65ede75293b82f3dfdc946d1c6cb
danielguarchaj <gtdanniel@gmail.com>
2018-05-13 20:47:48 -0600

    Finished video 12 - Writing another view

--- website/music/urls.py
+++ website/music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    #   music/
     url(r'^$', views.index, name='index'), # if the url is empty is being kinda like the index for each app
+
+    #   music/712 (album's id)
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
